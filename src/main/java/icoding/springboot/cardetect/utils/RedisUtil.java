29a12605bf2e5e620a3095e640fbf8ec6994078b
987654321-edragain <1372959677@qq.com>
2025-03-05 15:22:29 +0800

    RedisUtil

---
+++ src/main/java/icoding/springboot/cardetect/utils/RedisUtil.java
@@ -0,0 +1,25 @@
+package icoding.springboot.cardetect.utils;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+public class RedisUtil {
+    private static  final JedisPool jedisPool;
+
+    static {
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxTotal(10);
+        jedisPool = new JedisPool(poolConfig, "localhost", 6379);
+    }
+
+    public static Jedis getJedis() {
+        return jedisPool.getResource();
+    }
+
+    public static void close(Jedis jedis) {
+        if (jedis != null) {
+            jedis.close();
+        }
+    }
+}
